39ce87a4e3e99f3a405256cf29642aaedb7d1060
Roo-M <51594@edu.raseko.fi>
2024-02-06 15:18:21 +0200

    Created an object from Hooman class in the Main of Program class and tested it.

--- Luokkaharjoituksia/Program.cs
+++ Luokkaharjoituksia/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +9,61 @@ namespace Luokkaharjoituksia
 {
     class Hooman
     {
-        // Define properties of Hooman
-        string name;
-        int age;
-        string gender;
+        // Define properties of Hooman ie. fields
+        string name = "Essi Esimerkki";
+        int age = 30;
+        string gender = "Emäntä";
+
+        // Default constructor w/o arguments
+        // No need to define, will be created automatically
+        public Hooman()
+        {
+            
+        }
+
+        // Constructor with one argument
+        public Hooman(string name)
+        { 
+            this.name = name;
+        }
+
+        // Constructor with 2 arguments
+        public Hooman(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        // Constructor with 3 arguments
+        public Hooman(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+
+        }
+
+        // A method to say something
+        public void SayOpinion()
+        {
+            Console.WriteLine("Voi, lemmikit ne on elämän suola");
+        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Create (instantiate) a hooman object from Hooman class
+            Hooman owner = new Hooman("Ossi Omistaja", 35, "isäntä");
+
+            // Call the SayOpinion method
+            owner.SayOpinion();
+
+            // Keep the window open until entr is pressed
+
+            Console.ReadLine();
+
+
         }
     }
 }
